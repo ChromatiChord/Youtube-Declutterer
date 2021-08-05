cf515aca62642872394e4b09ae085f2ddbb7341f
ChromatiChord <callumward56@gmail.com>
2021-08-06 05:28:05 +1000

    cleared up function name and added more through commenting

--- Extension/content.js
+++ Extension/content.js
@@ -9,26 +9,30 @@ const icons = {
 }
  
 for (let icon in icons) {
-  disable_icon(icon);
+  icon_toggle(icon);
 } 
 
+// every 10 seconds, ensure the correct icons are disabled
 setInterval(function() {
   for (let icon in icons) {
-    disable_icon(icon);
+    icon_toggle(icon);
   } 
 }, 10000);
 
+// called by del_icon() in popup.js whenever a switch is flicked or slider adjusted
 chrome.runtime.onMessage.addListener(function (request) {
+  // if request is a number, use this to adjust playback speed
   if (!isNaN(request)) {
     document.getElementsByTagName("video")[0].playbackRate = parseFloat(request);  
   } 
   else {
     icons[request] = !icons[request];  
-    disable_icon(request);
+    icon_toggle(request);
   }
 });
 
-function disable_icon(icon) {
+// turns an icon on or off
+function icon_toggle(icon) {
   if (icon === '[title="Play on TV"]') {
     document.querySelector(icon).style.display = icons[icon] ? "inline" : "none";
   } else {
